138f2d224a5881efbfbba096c09c605061e98c26
Egor <egor.klopov2016@yandex.ru>
2025-10-31 19:30:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    """Функция складывает два числа."""
+    return a + b
+
+
+def multiply(a, b):
+    """Функция умножает два числа."""
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Сумма:", add(2, 3))
+    print("Произведение:", multiply(2, 3))
---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from main import add, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 10) == 0
+    assert multiply(-2, 3) == -6
